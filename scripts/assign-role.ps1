55e9eb372d44e8a60359d7b57ce2c5a9d5a4a394
harirajaj <76244832+harirajaj@users.noreply.github.com>
2021-10-11 14:54:12 +0530

    Update assign-role.ps1

--- scripts/assign-role.ps1
+++ scripts/assign-role.ps1
@@ -1,1 +1,3 @@
-
+New-AzRoleAssignment -ObjectId 5a598c95-b9f8-476f-8a23-c729ecfe6646 `
+-RoleDefinitionId 17d1049b-9a84-46fb-8f53-869881c3d3ab `
+-Scope "/subscriptions/1a0fe89f-51ab-4151-be36-d069115fcec6/resourceGroups/RFC_Dev_Test001/rfcstorageac001"
