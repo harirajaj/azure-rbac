ff859e69acadc513aa1c562998315c9243476849
harirajaj <76244832+harirajaj@users.noreply.github.com>
2021-10-11 15:14:54 +0530

    Update assign-role.ps1

--- scripts/assign-role.ps1
+++ scripts/assign-role.ps1
@@ -1,1 +1,1 @@
-New-AzRoleAssignment -ObjectId 2356b3fb-233b-4896-b85d-eace19a3b59d -RoleDefinitionId 17d1049b-9a84-46fb-8f53-869881c3d3ab -Scope "/subscriptions/1a0fe89f-51ab-4151-be36-d069115fcec6/resourceGroups/RFC_Dev_Test001/rfcstorageac001"
+New-AzRoleAssignment -ObjectId 2356b3fb-233b-4896-b85d-eace19a3b59d -RoleDefinitionId 17d1049b-9a84-46fb-8f53-869881c3d3ab -Scope "/subscriptions/1a0fe89f-51ab-4151-be36-d069115fcec6/resourceGroups/RFC_Dev_Test001/providers/Microsoft.Storage/storageAccounts/rfcstorageac001"
